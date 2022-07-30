d39d7f8b2e8d38caccea8a4aa2dd0cf24ea1bfd4
Michael Chinn <michael.j.chinn@gmail.com>
2022-07-30 21:19:02 +1200

    Update test name missed in previous cleanup commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders empty App', () => {
   render(<App />);
   const mainElement = screen.getByRole("main");
   expect(mainElement).toBeEmptyDOMElement();
